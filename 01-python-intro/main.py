4b84f367fbf974d17bd35dd7d344bf07f7d3fe6d
munbay23k-coder <munbay.23k@gmail.com>
2026-07-08 20:52:35 -0500

    primeros codigos

---
+++ 01-python-intro/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 01-python-intro!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from datascience-g8!")
+
+
+if __name__ == "__main__":
+    main()
